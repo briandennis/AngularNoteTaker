a592b3eb7f2f5eb9f72d75bd29d00ee4753f4da0
Brian Dennis <briandennis321@gmail.com>
2016-03-29 21:22:54 -0500

    fixed old files

--- built/app/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var browser_1 = require('angular2/platform/browser');
-var app_root_ts_1 = require('./app.root.ts');
-browser_1.bootstrap(app_root_ts_1.RootComponent);
-//# sourceMappingURL=main.js.map
