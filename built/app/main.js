fba69d9d6dabc668b916d8fa597dd3e37250cafb
Brian Dennis <briandennis321@gmail.com>
2016-03-29 20:08:45 -0500

    added rest of scafolding

---
+++ built/app/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var browser_1 = require('angular2/platform/browser');
+var app_root_ts_1 = require('./app.root.ts');
+browser_1.bootstrap(app_root_ts_1.RootComponent);
+//# sourceMappingURL=main.js.map
