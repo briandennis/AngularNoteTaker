8a447cdc4baaaf11ae147ebebb069ef9290743a4
Brian Dennis <briandennis321@gmail.com>
2016-03-29 21:21:06 -0500

    added stylesheets

--- app/app.root.js
+++ app/app.root.js
@@ -24,7 +24,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 RootComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
+                        template: '<h1>My First Angular 2 App</h1><h2>This is a test!</h2>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], RootComponent);
